2bb1a2aa4cfdf6fe1c302fff5ecaa0deef1edce8
Victor Espaillat <vicander_99@hotmail.com>
2020-05-16 13:44:58 -0400

    75. Sorting implementation (Bubble)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
-console.log(123);
+class Sorter {
+    // collection: number[];
+    // constructor(collection: number[]) {
+    //     this.collection = collection;
+    // }
 
-const logSomething = () => {
-    console.log("something");
-};
+    /* Equivalent to above */
+    constructor(public collection: number[]) {}
 
-logSomething();
+    /* bubble sort */
+    sort(): void {
+        let { length } = this.collection;
+
+        for (let i = 1; i < length; i++) {
+            for (let j = 0; j < length - i; j++) {
+                // console.log(`${this.collection[j]} < ${this.collection[j+1]}`);
+                if (this.collection[j] > this.collection[j + 1]) {
+                    // console.log("--SWAP--");
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
